fe6380db16fd04bf4d33396655013f3e85455dbf
umarmansoor001 <90560001+umarmansoor001@users.noreply.github.com>
2021-12-06 21:59:44 +0500

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,11 +12,6 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-//			BorderPane root = new BorderPane();
-//			Scene scene = new Scene(root,400,400);
-//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-//			primaryStage.setScene(scene);
-//			primaryStage.show();
 			Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
 			Scene scene = new Scene(root);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
